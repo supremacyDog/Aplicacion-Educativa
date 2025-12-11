efd84e7d672daf9a6228601c5f938291e583e274
doggy64 <rickyespanol8@gmail.com>
2025-12-11 15:27:19 -0400

    change:modificacion del server para produccion

--- src/server.js
+++ src/server.js
@@ -38,7 +38,11 @@ app.use("/components", express.static("src/views/components"));
 app.use("/pages", express.static("src/views/pages"));
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
   ssl: { rejectUnauthorized: false }
 });
 
